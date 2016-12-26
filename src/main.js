6bb659f95669c2086a9d4f952215667b9a2fe89d
wjl <435821970@qq.com>
2016-12-26 12:46:49 +0800

    20161226.1

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './App';
-import goods from './components/goods/goods';
-import ratings from './components/ratings/ratings';
-import seller from './components/seller/seller';
+import goods from 'components/goods/goods';
+import ratings from 'components/ratings/ratings';
+import seller from 'components/seller/seller';
+
+import 'common/stylus/index.styl';
 Vue.use(VueRouter);
 
 let app = Vue.extend(App);
@@ -23,3 +25,4 @@ router.map({
 });
 
 router.start(app, '#app');
+router.go('/goods');
