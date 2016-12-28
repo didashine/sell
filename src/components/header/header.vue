b6bdb393287842100c5dfe6eebc20de9cb3c5b33
wjl <435821970@qq.com>
2016-12-28 09:43:49 +0800

    20161228

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -17,16 +17,26 @@
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
-      <div v-if="seller.supports"class="support-count">
+      <div v-if="seller.supports"class="support-count" @click="showDetail">
         <span class="count">{{seller.supports.length}}个</span>
         <i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
-    <div class="bulletin-wrapper">
+    <div class="bulletin-wrapper" @click="showDetail">
       <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span><i class="icon-keyboard_arrow_right"></i>
     </div>
     <div class="background">
       <img width='100%' height='100%' :src="seller.avatar">
+    </div>
+    <div v-show="detailShow"class="detail">
+      <div class="detail-wrapper clearfix">
+        <div class="detail-main">
+          <h1 class="name">{{seller.name}}</h1>
+        </div>
+      </div>
+      <div class="detail-close">
+        <i class="icon-close"></i>
+      </div>
     </div>
   </div>
 </template>
@@ -35,6 +45,16 @@
     props: {
       seller: {
         type: Object
+      }
+    },
+    data() {
+      return {
+        detailShow:false
+      }
+    },
+    methods: {
+      showDetail() {
+        this.detailShow = true
       }
     },
     created() {
@@ -47,6 +67,7 @@
   @import "../../common/stylus/mixin.styl"
   .header
     color: #ffffff
+    overflow: hidden
     background-color: rgba(7,17,27,0.5)
     position: relative
     .content-wrapper
@@ -130,7 +151,7 @@
       background: rgba(7,17,27,0.2)
       .bulletin-title
         display: inline-block
-        margin-top: 7px
+        margin-top: 8px
         vertical-align: top
         width: 22px
         height: 12px
@@ -154,4 +175,32 @@
       height: 100%
       z-index: -1
       filter: blur(10px)
+    .detail
+      position:fixed
+      z-index: 100
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      overflow:outo
+      background:rgba(7,17,27,0.8)
+      .detail-wrapper
+        min-height:100%
+        width:100%
+        .detail-main
+          margin-top: 64px
+          padding-bottom: 64px
+          .name
+            font-size: 16px
+            line-height: 16px
+            font-weight: 700
+            text-align:center
+            margin-bottom: 16px
+      .detail-close
+        position:relative
+        width: 32px
+        height: 32px
+        margin: -64px auto 0 auto
+        clear: both
+        font-size: 32px
 </style>
---
+++ src/components/star/star.vue
@@ -0,0 +1,11 @@
+<template>
+
+</template>
+
+<script type="ecmascript-6">
+
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
